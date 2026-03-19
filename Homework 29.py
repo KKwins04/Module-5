0c2507c185308e3ac2b365dba6b7d1da5821284e
KKwins04 <arvind_kavin_karthik@students.edu.sg>
2026-03-19 15:31:47 +0800

    Homework 29 commit

--- Homework 29.py
+++ Homework 29.py
@@ -2,9 +2,11 @@ class stringReverse:
     def __init__(self, str):
         self.str = str
 
-    def reverse(str):
-        words = str.split()
-        print(words)
+    def reverse(self):
+        words = self.str.split()
+        reversed_words = words[::-1]
+        print(" ".join(reversed_words))
 
-s2 = stringReverse("I like coding very much.")
+
+s2 = stringReverse("I like coding very much .")
 s2.reverse()
