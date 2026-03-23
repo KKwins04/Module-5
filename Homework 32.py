071ed56c9f67854e5f497c45dc9e01f92d032bea
KKwins04 <arvind_kavin_karthik@students.edu.sg>
2026-03-23 14:41:58 +0800

    Homework 32 commit

---
+++ Homework 32.py
@@ -0,0 +1,27 @@
+class RomanConverter:
+
+    def __init__(self, number):
+        self.number = number
+
+    def convert(self):
+        num = self.number
+        val = [1000, 900, 500, 400, 100, 90,
+               50, 40, 10, 9, 5, 4, 1]
+        syb = ["M", "CM", "D", "CD", "C", "XC",
+               "L", "XL", "X", "IX", "V", "IV", "I"]
+
+        roman = ""
+        i = 0
+
+        while num > 0:
+            for _ in range(num // val[i]):
+                roman += syb[i]
+                num -= val[i]
+            i += 1
+
+        return roman
+
+
+number = int(input("Enter a number: "))
+obj = RomanConverter(number)
+print(obj.convert())
